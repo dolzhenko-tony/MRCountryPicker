10498b733cee176dcb1b5c6539fd4d67d872b689
Anton Dolzhenko <dolzhenko.tony@gmail.com>
2021-12-27 16:32:23 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,10 +5,14 @@ import PackageDescription
 let package = Package(
     name: "MRCountryPicker",
     products: [
-        .library(name: "MRCountryPicker", targets: ["MRCountryPicker"])
+        .library(name: "MRCountryPicker",
+                 targets: ["MRCountryPicker"])
     ],
     targets: [
-        .target(name: "MRCountryPicker", path: "MRCountryPicker", exclude: [])
+        .target(name: "MRCountryPicker",
+                path: "MRCountryPicker",
+                resources: [.process("MRCountryPicker/Assets/SwiftCountryPicker.bundle")],
+                exclude: [])
     ],
     swiftLanguageVersions: [.v5]
 )
